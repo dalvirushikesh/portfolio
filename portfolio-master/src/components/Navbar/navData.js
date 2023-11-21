b9e55b1a7e0d07cda29b29e9f6eb19d436a429ac
Rushikesh Dalvi <dalvi.ru@northeastern.edu>
2023-11-20 20:25:35 -0500

    portfolio

---
+++ portfolio-master/src/components/Navbar/navData.js
@@ -0,0 +1,36 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Details',
+    pathname: '/#details',
+  },
+  {
+    label: 'Articles',
+    pathname: '/articles',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/RUSHIKESHD19546',
+    icon: 'twitter',
+  },
+  {
+    label: 'Figma',
+    url: 'https://www.figma.com/file/Nd8y17XGKGFTDtk4AIskun/ParkHub?type=design&mode=design&t=QokJEtAmV6tiUcoG-1',
+    icon: 'figma',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/dalvirushikesh',
+    icon: 'github',
+  },
+];
---
+++ portfolio-master/src/layouts/Home/Home.js
@@ -0,0 +1,154 @@
+import gamestackTexture2Large from 'assets/HT-Bangla.png';
+import gamestackTexture2Placeholder from 'assets/gamestack-list-placeholder.jpg';
+import gamestackTexture2 from 'assets/HT-Bangla.png';
+import gamestackTextureLarge from 'assets/HT-SignIn.png';
+import gamestackTexturePlaceholder from 'assets/gamestack-login-placeholder.jpg';
+import gamestackTexture from 'assets/HT-SignIn.png';
+import sliceTextureLarge from 'assets/slice-app-large.jpg';
+import sliceTexturePlaceholder from 'assets/slice-app-placeholder.jpg';
+import sliceTexture from 'assets/slice-app.jpg';
+import sprTextureLarge from 'assets/uncover.png';
+import sprTexturePlaceholder from 'assets/spr-lesson-builder-dark-placeholder.jpg';
+import sprTexture from 'assets/uncover.png';
+import { Footer } from 'components/Footer';
+import { Meta } from 'components/Meta';
+import { Intro } from 'layouts/Home/Intro';
+import { Profile } from 'layouts/Home/Profile';
+import { ProjectSummary } from 'layouts/Home/ProjectSummary';
+import { useEffect, useRef, useState } from 'react';
+import styles from './Home.module.css';
+
+const disciplines = ['Designer', 'Prototyper', 'Illustrator', 'Developer', 'Gamer'];
+
+export const Home = () => {
+  const [visibleSections, setVisibleSections] = useState([]);
+  const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+  const intro = useRef();
+  const projectOne = useRef();
+  const projectTwo = useRef();
+  const projectThree = useRef();
+  const details = useRef();
+
+  useEffect(() => {
+    const sections = [intro, projectOne, projectTwo, projectThree, details];
+
+    const sectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const section = entry.target;
+            observer.unobserve(section);
+            if (visibleSections.includes(section)) return;
+            setVisibleSections(prevSections => [...prevSections, section]);
+          }
+        });
+      },
+      { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
+    );
+
+    const indicatorObserver = new IntersectionObserver(
+      ([entry]) => {
+        setScrollIndicatorHidden(!entry.isIntersecting);
+      },
+      { rootMargin: '-100% 0px 0px 0px' }
+    );
+
+    sections.forEach(section => {
+      sectionObserver.observe(section.current);
+    });
+
+    indicatorObserver.observe(intro.current);
+
+    return () => {
+      sectionObserver.disconnect();
+      indicatorObserver.disconnect();
+    };
+  }, [visibleSections]);
+
+  return (
+    <div className={styles.home}>
+      <Meta
+        title="Software Developer"
+        description="Design portfolio of Rushikesh Dalvi — a Software Developer working on web & mobile
+          apps with a focus on motion,performance, experience design, and accessibility."
+      />
+      <Intro
+        id="intro"
+        sectionRef={intro}
+        disciplines={disciplines}
+        scrollIndicatorHidden={scrollIndicatorHidden}
+      />
+      <ProjectSummary
+        id="project-1"
+        sectionRef={projectOne}
+        visible={visibleSections.includes(projectOne.current)}
+        index={1}
+        title="Designing the future of education"
+        description="Designing a platform to help educators build better online quizzes and lessons"
+        buttonText="View project"
+        buttonLink="/projects/smart-sparrow"
+        model={{
+          type: 'laptop',
+          alt: 'Smart Sparrow lesson builder',
+          textures: [
+            {
+              srcSet: [sprTexture, sprTextureLarge],
+              placeholder: sprTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-2"
+        alternate
+        sectionRef={projectTwo}
+        visible={visibleSections.includes(projectTwo.current)}
+        index={2}
+        title="Hindustan Times Bangla News "
+        description="Design and development for a news website built in React and Next.js"
+        buttonText="View website"
+        buttonLink="https://bangla.hindustantimes.com/"
+        model={{
+          type: 'phone',
+          alt: 'App login screen',
+          textures: [
+            {
+              srcSet: [gamestackTexture, gamestackTextureLarge],
+              placeholder: gamestackTexturePlaceholder,
+            },
+            {
+              srcSet: [gamestackTexture2, gamestackTexture2Large],
+              placeholder: gamestackTexture2Placeholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-3"
+        sectionRef={projectThree}
+        visible={visibleSections.includes(projectThree.current)}
+        index={3}
+        title="AT&T wireless device"
+        description="Developed a web app to help AT&T customers find the right wireless device"
+        buttonText="View project"
+        buttonLink="https://www.att.com/wireless/"
+        model={{
+          type: 'laptop',
+          alt: 'AT&T wireless device',
+          textures: [
+            {
+              srcSet: [sliceTexture, sliceTextureLarge],
+              placeholder: sliceTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <Profile
+        sectionRef={details}
+        visible={visibleSections.includes(details.current)}
+        id="details"
+      />
+      <Footer />
+    </div>
+  );
+};
